1c173392ffaee93e7a9e415ddc50894d7b2c77de
Artem Chernousov <artem.chernousov@jjj.work>
2025-04-12 21:16:52 +0400

    add scroll animation

--- circularProgressBar/circularProgressBar/MoneyCounterView/NumberCounterView.swift
+++ circularProgressBar/circularProgressBar/MoneyCounterView/NumberCounterView.swift
@@ -12,9 +12,12 @@ struct NumberCounterView: View {
         static let minValue = 0
         static let maxValue = 9
         static let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        static let scrollingNumberColor = Color(red: 229 / 255, green: 229 / 255, blue: 229 / 255)
     }
     
     let value: Int
+    
+    @State private var isScrollingStarted: Bool = false
     
     var body: some View {
         ScrollViewReader { proxy in
@@ -25,17 +28,24 @@ struct NumberCounterView: View {
                             .font(.largeTitle)
                             .bold()
                             .id(number)
+                            .foregroundStyle(isScrollingStarted ? Constants.scrollingNumberColor : .black)
                     }
                 }
             }
             .frame(maxWidth: 25, maxHeight: 50)
             .scrollDisabled(true)
             .onChange(of: value) {
+                isScrollingStarted = true
+                
                 if value == 0 {
                     proxy.scrollTo(value)
+                    
+                    isScrollingStarted = false
                 } else {
                     withAnimation(.easeOut(duration: 0.6)) {
                         proxy.scrollTo(value)
+                        
+                        isScrollingStarted = false
                     }
                 }
             }
