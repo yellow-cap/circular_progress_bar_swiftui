320da09ed5f1a00d46b8fdb29120c1720c3cbfa5
Artem Chernousov <artem.chernousov@jjj.work>
2025-04-12 10:31:49 +0400

    create project

---
+++ circularProgressBar/circularProgressBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  circularProgressBar
+//
+//  Created by Artem Chernousov on 12.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ circularProgressBar/circularProgressBar/circularProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  circularProgressBarApp.swift
+//  circularProgressBar
+//
+//  Created by Artem Chernousov on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct circularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
